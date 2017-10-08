cb5ea4fb80da90d6fee244020a372cd4a7c0c9ac
Chuzs <291822616@qq.com>
2017-10-08 21:59:02 +0800

    添加底部组件

--- src/js/components/pc_index.js
+++ src/js/components/pc_index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import PCHeader from './pc_header';
+import PCFooter from './pc_footer';
 
 export default class PCIndex extends React.Component {
-	render(){
-		return (
-			<div>
-				<PCHeader></PCHeader>
-			</div>
+    render(){
+        return (
+            <div>
+                <PCHeader/>
+                <PCFooter/>
+            </div>
+        );
+    };
 
-		)
-	}
 }
+
+
